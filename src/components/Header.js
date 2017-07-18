326e5b28656989135065ae20183a271433505b8e
Mustafa Sertac Turkel <sertac.turkel@etstur.com>
2017-07-18 15:24:28 +0300

    Milestone 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,12 +9,11 @@ export const Header = (props) => {
         <div className="App">
             <nav className="pt-navbar .modifier pt-dark">
                 <div className="pt-navbar-group pt-align-left">
-                    <div className="pt-navbar-heading">Blueprint</div>
-                    <input className="pt-input" placeholder="Search files..." type="text" />
+                    <div className="pt-navbar-heading">Report Application Template</div>
+                    <button className="pt-button pt-minimal pt-icon-vertical-bar-chart-desc">Select Report</button>
                 </div>
                 <div className="pt-navbar-group pt-align-right">
-                    <button className="pt-button pt-minimal pt-icon-home">Home</button>
-                    <button className="pt-button pt-minimal pt-icon-document">Files</button>
+
                     <span className="pt-navbar-divider"></span>
                     <button className="pt-button pt-minimal pt-icon-user"></button>
                     <button className="pt-button pt-minimal pt-icon-notifications"></button>
